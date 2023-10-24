46da51ce7296414d4ea30c829ad27f158aeecdcf
eefuwape <eefuwape@gmail.com>
2023-10-24 01:39:21 -0400

    created hamster class

---
+++ javaScriptClassesHW3.js
@@ -0,0 +1,21 @@
+// Hamster Class
+
+class Hamster {
+    constructor(name) {
+        this.owner = ''
+        this.name = name
+        this.price = 15
+    }
+
+    wheelRun() {
+        console.log('squeak squeak')
+    }
+
+    eatFood() {
+        console.log('nibble nibble')
+    }
+
+    getPrice() {
+        console.log(this.price)
+    }
+}
