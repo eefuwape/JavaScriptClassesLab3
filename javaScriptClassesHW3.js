2b2908b72c91da9b1013746009ea7c07220f16be
eefuwape <eefuwape@gmail.com>
2023-10-24 03:12:24 -0400

    created person class

--- javaScriptClassesHW3.js
+++ javaScriptClassesHW3.js
@@ -19,3 +19,54 @@ class Hamster {
         console.log(this.price)
     }
 }
+
+class Person {
+    constructor(name) {
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamster = []
+        this.bankAccount = 0
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getAge() {
+        return this.age
+    }
+
+    getWeight() {
+        return this.weight
+    }
+    
+    greet() {
+        return 'Greetings ' + this.name + '!'
+    }
+
+    eat() {
+        this.weight++
+        this.mood++
+    }
+
+    exercise() {
+        this.weight--
+    }
+
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
+    }
+
+    buyHamster(hamster) {
+        const newHamster = new Hamster(name)
+        this.hamster.push(newHamster)
+        this.mood += 10
+        this.bankAccount -= getPrice()
+    }
+}
